3f36e8017c0cdfcb651b7892c2c995e3fd5d4035
Lemao <lemaoliu@gmail.com>
2018-01-29 21:03:33 +0800

    Update 24_points.py

--- 24_points.py
+++ 24_points.py
@@ -28,11 +28,14 @@ class Node:
       return "(%s %s %s)" % (self.lch.__str__(),
           self.opt, self.rch.__str__())
 
-def solve(a):
+def solve(a, tot):
+  """
+  search a computational tree such that its value is tot and all leaves are uniq from the list (a).
+  """
   if len(a) == 1:
     flag = 1 if abs(a[0].val-tot) < 1e-5 else 0
     return flag, a[0]
-  expr = None
+  node = None
   find_res = False
   for i, x in enumerate(a):
     for j in xrange(i+1,len(a)):
@@ -44,12 +47,12 @@ def solve(a):
         if opt == x.div:
           if w.val == 0 or u.val % w.val != 0:
             continue
-        find_res, expr = solve(res+[opt(u,w)])
+        find_res, node = solve(res+[opt(u,w)])
         if find_res:
           break
     if find_res:
       break
-  return find_res, expr
+  return find_res, node
  
 a = [1,2,2,6]
 a = [1,3,5,7]
@@ -58,7 +61,7 @@ a = [6,1,7,8]
 a = [13, 3, 2, 10, 1]
 a = [4,6,7,9]
 nodes = [Node(x) for x in a]
-ans, tree = solve(nodes)
+ans, tree = solve(nodes, 24)
 if ans:
   print tree, '= %d'%tot
 else:
